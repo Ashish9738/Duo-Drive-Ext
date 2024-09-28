c40226f46d4854c5294cbc28bc076e85b3c36d1d
Ashish9738 <ayadav9738@gmail.com>
2024-09-28 14:36:00 +0530

    Added Loader and removed  scrollBar

--- frontend/src/ui/Widget.jsx
+++ frontend/src/ui/Widget.jsx
@@ -1,32 +1,47 @@
 import Voice from "../components/Voice";
 import Button from "../components/Button";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 import { URL } from "../utils/constant";
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 
 const Widget = () => {
   const [input, setInput] = useState("");
+  const [loader, setLoader] = useState(false);
   const [history, setHistory] = useState([]);
+
+  const endOfMessagesRef = useRef(null);
 
   const handleChat = async () => {
     const data = {
       prompt: input,
     };
+    setLoader(true);
     try {
       const response = await axios.post(`${URL}/chat`, data);
       const newHistory = [...history, ...response.data.history];
       setHistory(newHistory);
       setInput("");
+      setLoader(false);
     } catch (error) {
+      setLoader(false);
       console.error("Failed to get the error", error);
     }
   };
 
+  useEffect(() => {
+    if (endOfMessagesRef.current) {
+      endOfMessagesRef.current.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, [history]);
+
   return (
-    <div className="h-[300px] w-[500px] bg-black relative flex flex-col">
+    <div className="h-[300px] w-[500px] bg-black relative flex flex-col ">
       {/* Response Display */}
-      <div className="flex-grow overflow-y-auto p-2">
+      <div className="flex-grow  p-2 overflow-y-scroll no-scrollbar">
         {history.map((entry, index) =>
           entry.parts.map((part, partIndex) => (
             <div
@@ -43,6 +58,7 @@ const Widget = () => {
             </div>
           ))
         )}
+        <div ref={endOfMessagesRef} />
       </div>
 
       {/* Bottom Portion [Input Area] */}
@@ -54,7 +70,7 @@ const Widget = () => {
           onChange={(e) => setInput(e.target.value)}
           value={input}
         />
-        <Button onClick={handleChat} />
+        {loader ? <Loader /> : <Button onClick={handleChat} />}
       </div>
     </div>
   );
