bc360ffb3263446ce57f0df0de93bf79e3159936
Ashish9738 <ayadav9738@gmail.com>
2024-09-30 22:18:32 +0530

    Added cors origin

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,11 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(
   session({
