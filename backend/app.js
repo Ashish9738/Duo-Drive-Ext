12a53bc7aef3c01131510bb57fce7feba6f2ef08
Ashish9738 <ayadav9738@gmail.com>
2024-09-30 23:55:47 +0530

    Added cors for chrome ext

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", /^chrome-extension:\/\/.+/],
   })
 );
 app.use(express.json());
